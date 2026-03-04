17b716b537f417774cdc6eb80b8dfaf60cb792d8
NinjaElite24 <156318142+NinjaElite24@users.noreply.github.com>
2026-03-04 14:59:08 +0100

    moar

--- Assets/LOD.cs
+++ Assets/LOD.cs
@@ -14,6 +14,8 @@ public class LOD : MonoBehaviour
             if (D < dis) lev++;
         }
 
+        lev = Mathf.Clamp(lev, 0, Dist.Length-1);
+
         if (lev != Lev)
         {
             LODLevels[Lev].SetActive(false);
