9ae68fa7c4087d2bf8791556c7fcf73f91007a34
Algot2 <156318062+Algot2@users.noreply.github.com>
2026-03-06 00:39:27 +0100

    Decoredted otside

--- Assets/LOD.cs
+++ Assets/LOD.cs
@@ -6,21 +6,17 @@ public class LOD : MonoBehaviour
     public float[] Dist;
     public int Lev = 0;
     public bool randomise;
+    public bool ForeTranssform;
 
     void Start() {
-        if (randomise) {
-            Vector3 S = transform.localScale;
-            transform.localScale = new Vector3 { 
-                x = S.x * Random.Range(0.9f,1.1f),
-                y = S.y * Random.Range(0.9f,1.1f),
-                z = S.z * Random.Range(0.9f,1.1f)
-            } * Random.Range(0.8f, 1.2f);
-
-            transform.rotation *= Quaternion.Euler(0, Random.Range(-180, 180), 0);
+        if (ForeTranssform) {
+            LODLevels[1].transform.position = LODLevels[0].transform.position;
+            LODLevels[1].transform.rotation = LODLevels[0].transform.rotation;
+            LODLevels[1].transform.localScale = LODLevels[0].transform.localScale;
         }
     }
     void Update() {
-        float dis = Vector3.Distance(transform.position, Camera.main.transform.position);
+        float dis = Vector3.Distance(LODLevels[0].transform.position, Camera.main.transform.position);
 
         int lev = 0;
         foreach (float D in Dist) {
