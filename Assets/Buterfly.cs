f20c41eb7d4d7d6f0961474e357cc65abb6f77f2
Algot2 <156318062+Algot2@users.noreply.github.com>
2026-03-20 12:06:57 +0100

    Vishols

---
+++ Assets/Buterfly.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Buterfly : MonoBehaviour
+{
+    Vector3 Rotpont = Vector3.zero;
+    public float prefurdDis = 5;
+    public float seed = 10;
+    public int dir = -1;
+    Vector3 pikeNewPos() {
+        Vector3 pl = GameManager.player.position;
+
+        return pl + new Vector3 {
+            x = Random.Range(-1f, 1f),
+            y = Random.Range(0, 0.5f),
+            z = Random.Range(-1f, 1f)
+        }.normalized * Random.Range(0, 10);
+    }
+    private void Start()
+    {
+        dir = Random.Range(0, 2) == 0 ? -1 : 1;
+        seed = Random.Range(5, 10);
+        prefurdDis = Random.Range(5f, 10f);
+
+        transform.position = pikeNewPos();
+    }
+    void Update() {
+
+        Vector3 dis = Rotpont - transform.position;
+        if (Vector3.Distance(Rotpont, GameManager.player.transform.position) > 10) {
+            Rotpont = pikeNewPos();
+        }
+        Vector3 move = Vector3.Cross(dis.normalized, transform.up) * dir;
+        if (Mathf.Abs(dis.magnitude - prefurdDis) > 1) 
+            move += dis.normalized * (dis.magnitude < prefurdDis ? -1 : 1);
+        transform.forward = Vector3.Lerp(transform.forward, move, Time.deltaTime * 10);
+        transform.position += move * Time.deltaTime * seed;
+
+        if (Physics.Raycast(transform.position + Vector3.up * 5, Vector3.down, out var hit, 100, GameManager.Instance.ground)
+           && hit.point.y - transform.position.y > 0)
+        {
+            transform.position += Vector3.up * Time.deltaTime * 10;
+
+        }
+    }
+}
